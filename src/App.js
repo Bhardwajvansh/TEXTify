2d2470cdababe410064447d8110ef163b7e7d76b
Vansh <vanshayushi@gmail.com>
2022-08-16 18:41:36 +0530

    router dom commented

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from "react";
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import Textarea from "./Components/Textarea";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
     {/* npm install react-router-dom */}
-      <Router>
+      {/* <Router> */}
         <Navbar/>
-        <Routes>   
-          <Route
-            path="/"
-            element={<Textarea heading="Enter text to analyze" />}
-          />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+        {/* <Routes>   
+          <Route */}
+            {/* path/="/" */}
+            <Textarea heading="Enter text to analyze" />
+            {/* element={} */}
+          {/* /> */}
+          {/* <About /> */}
+          {/* <Route path="/about" element={} /> */}
+        {/* </Routes> */}
+      {/* </Router> */}
     </>
   );
 }
